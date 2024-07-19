f3a8f3d8995f26183e2faa26edc4206ae9301c5f
Luis Dourado <lfaddourado@gmail.com>
2024-07-19 16:42:44 -0300

    feat(users): add handler for user creation

---
+++ handler_user.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/Lutefd/aggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `json:"name"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	var params parameters
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "error parsing request")
+		return
+	}
+	user, err := cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error creating user")
+		return
+	}
+	respondWithJSON(w, http.StatusCreated, user)
+}
