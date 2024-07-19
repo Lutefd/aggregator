fc999134121452737d885331c9ccb5e0fd389619
Luis Dourado <lfaddourado@gmail.com>
2024-07-19 17:02:50 -0300

    feat(users): add get user by api key handler

--- handler_user.go
+++ handler_user.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/Lutefd/aggregator/internal/auth"
 	"github.com/Lutefd/aggregator/internal/database"
 	"github.com/google/uuid"
 )
@@ -30,5 +31,19 @@ func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) 
 		respondWithError(w, http.StatusInternalServerError, "error creating user")
 		return
 	}
-	respondWithJSON(w, http.StatusCreated, user)
+	respondWithJSON(w, http.StatusCreated, databaseUserToUser(user))
+}
+
+func (cfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+	user, err := cfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, databaseUserToUser(user))
 }
