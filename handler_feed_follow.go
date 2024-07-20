77b234ad9176d4d36694e0e18ded31e39eb54916
Luis Dourado <lfaddourado@gmail.com>
2024-07-20 14:51:23 -0300

    feat(feeds): add feed follow creation handler and route

---
+++ handler_feed_follow.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Lutefd/aggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		FeedID uuid.UUID `json:"feed_id"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	var params parameters
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "error parsing request")
+		return
+	}
+	feedFollows, err := cfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		UserID:    user.ID,
+		FeedID:    params.FeedID,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("error following feed: %s", err))
+		return
+	}
+	respondWithJSON(w, http.StatusCreated, databaseFeedFollowToFeedFollow(feedFollows))
+}
